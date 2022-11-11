9fabdf33295b78795bcda37f526d296c54dd600f
Neo Shao Hong <64857033+git-of-neo@users.noreply.github.com>
2022-11-11 07:53:46 +0000

    implemented container notes cross talk

--- Dockerfile
+++ Dockerfile
@@ -10,5 +10,7 @@ RUN pip install --no-cache-dir --upgrade -r /requirements.txt
 
 COPY ./app .
 
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "3000", "--reload"]
+# CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "3000", "--reload"]
 # CMD ["pwd"]
+
+# EXPOSE 3001
--- app/main.py
+++ app/main.py
@@ -3,9 +3,27 @@ from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
 from typing import List
 from jinja2 import Environment, FileSystemLoader
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 environment = Environment(loader = FileSystemLoader("templates/"))
+
+# list of allowed origins
+origins = [
+    "http://localhost:3000",
+    "http://localhost:3001",
+    "http://localhost:3002",
+]
+
+# allow cross origin access
+# reference : https://fastapi.tiangolo.com/tutorial/cors/
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True, # allow cookies
+    allow_methods=["*"], # allow all methods
+    allow_headers=["*"], # allow all headers
+)
 
 class Row(BaseModel):
     data : List[int]
@@ -16,6 +34,9 @@ class Col(BaseModel):
 # result of multiplication
 class Point(BaseModel):
     value : int
+
+class Matrix(BaseModel):
+    data : List[List[int]]
 
 @app.get("/", response_class = HTMLResponse)
 async def root():
@@ -29,6 +50,29 @@ async def compute(row: Row, col: Col):
     res = sum([row.data[i] * col.data[i] for i in range(len(row.data))])
     return {"value" : res}
 
+def is_uniform_row(matrix):
+    row_size = lambda row : len(row)
+    x = list(map(row_size, matrix))
+    for i in range(len(x) - 1):
+        if x[i]!=x[i+1]:
+            return False
+    return True
+
+@app.post("/api/multiply_matrix", response_model = Matrix)
+# dot product
+async def multiply_matrix(m1 : Matrix, m2 : Matrix):
+    matrix1 = m1.data
+    matrix2 = m2.data
+    # validate matrices 
+    if not (is_uniform_row(matrix1) and is_uniform_row(matrix2) and ( len(matrix1[0]) == len(matrix2) ) ):
+        raise HTTPException(status_code = 400, detail = "matrix dimensions must match")
+    
+    # distribute workload (point based workload distribution, there are faster algorithms like fox algo but stick to basic for now)
+    
+
+    pass
+
+# testing if hot reload is working as aspected
 # @app.get("/something")
 # async def something():
 #     return {
